b4f5e2e70850f44dad085cee2579d04d393385fd
DELL53M6XZ1\Administrateur <zoua1703@usherbrooke.ca>
2017-09-22 17:37:16 -0400

    no message

---
+++ crmApp-backend/crmApp-back/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	
+	console.log("Test backend");
+	
+	res.render('index', {
+		title : 'Express'
+			
+	});
+});
+
+module.exports = router;
--- crmApp-frontend/src/App.js
+++ crmApp-frontend/src/App.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var jeremy ="";
+
 class App extends Component {
+
+	 fetchData(){
+	    const url = 'http://localhost:3002';
+	    fetch(url)
+	    .then (response => {console.log("response: reception: ", response  )})
+	    .catch( error => {console.error("grosse erreur les gars !", error)});
+	}
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="App-header"   >
+          <img src={logo} className="App-logo" alt="logo"  />
+          <h2> {jeremy}</h2>
+          
+          <button onClick={this.fetchData} >
+          "Fetch Data"
+          </button>
+
+          
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
