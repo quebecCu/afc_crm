e6dd6372205dbdd75106de8628809b1acc158713
vince8lte <v.quernel@gmail.com>
2017-10-06 12:35:46 -0400

    script BD users

--- crmApp-backend/Database/Create_DB_users.sql
+++ crmApp-backend/Database/Create_DB_users.sql
@@ -12,3 +12,55 @@ DROP TABLE IF EXISTS "OPERATION" CASCADE;
 DROP TABLE IF EXISTS "MENU" CASCADE;
 DROP TABLE IF EXISTS "TAB" CASCADE;
 DROP TABLE IF EXISTS "CHAMP" CASCADE;
+DROP TABLE IF EXISTS "TYPE" CASCADE;
+
+CREATE TABLE public."ROLEADM" (
+  idrole serial PRIMARY KEY,
+  description  varchar(30)
+);
+
+CREATE TABLE public."UTILISATEUR" (
+  iduser serial PRIMARY KEY,
+  login  varchar(20),
+  password  varchar(20),
+  idrole  integer REFERENCES "ROLEADM" (idrole)
+);
+
+CREATE TABLE public."MENU" (
+  idmenu serial PRIMARY KEY,
+  description  varchar(30)
+);
+
+CREATE TABLE public."OPERATION" (
+  idoperation serial PRIMARY KEY,
+  description  varchar(30)
+);
+
+CREATE TABLE public."PERMISSION" (
+  idpermission serial PRIMARY KEY,
+  idrole  integer REFERENCES "ROLEADM" (idrole),
+  idmenu  integer REFERENCES "MENU" (idmenu),
+  idoperation  integer REFERENCES "OPERATION" (idoperation)
+);
+
+CREATE TABLE public."TAB" (
+  idtab serial PRIMARY KEY,
+  description  varchar(30),
+  idmenu  integer REFERENCES "MENU" (idmenu)
+);
+
+CREATE TABLE public."TYPE" (
+  idtype serial PRIMARY KEY,
+  description  varchar(30)
+);
+
+CREATE TABLE public."CHAMP" (
+  idchamp serial PRIMARY KEY,
+  idtab  integer REFERENCES "TAB" (idtab),
+  idtype  integer REFERENCES "TYPE" (idtype),
+  description  varchar(30),
+  priorité  integer,
+  obligatoire  boolean,
+  tab  varchar(30),
+  nom  varchar(30)
+);
