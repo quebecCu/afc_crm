873caaf6fdd6d6c31749adf9e7952a336082988d
devonbrunet <devon.brunet@insa-lyon.fr>
2017-10-24 00:22:08 -0400

    Merge

--- crmApp-backend/crmApp-back/app.js
+++ crmApp-backend/crmApp-back/app.js
@@ -7,18 +7,13 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var login = require('./routes/login');
-<<<<<<< HEAD
-=======
 var reset = require('./routes/reset');
 var fournisseurs = require('./routes/fournisseurs');
 var assurancesCollectives = require('./routes/assurancesCollectives');
->>>>>>> branch 'front-end-maquettes' of https://minla.dinf.usherbrooke.ca/projets-a17/eq14.git
 var app = express();
-<<<<<<< HEAD
 var database = require('./models/database');
 var bcrypt = require('bcrypt');
 
-=======
  
 >>>>>>> branch 'front-end-maquettes' of https://minla.dinf.usherbrooke.ca/projets-a17/eq14.git
 //view engine setup 
@@ -33,15 +28,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/', login);
-<<<<<<< HEAD
-=======
 app.use('/', reset);
 app.use('/', assurancesCollectives);
 app.use('/', fournisseurs);
->>>>>>> branch 'front-end-maquettes' of https://minla.dinf.usherbrooke.ca/projets-a17/eq14.git
 
 //catch 404 and forward to error handler
 app.use(function(req, res, next) {
