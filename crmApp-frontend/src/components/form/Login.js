bd3b77abbdedcb21f236943eb41fcf3d0402fe62
Aziz ZOUAOUI <zoua1703@usherbrooke.ca>
2017-10-28 17:48:38 -0400

    Responsive Login: Iphone6, Iphone 6 plus, Ipad

--- crmApp-frontend/src/components/form/Login.js
+++ crmApp-frontend/src/components/form/Login.js
@@ -1,18 +1,21 @@
 //import MyInput from './../components/Input';
 import React, { Component } from 'react';
 import '../../style/Login.css';
+import  logoCo from '../../style/images/logoCompany.jpg';
+import  '../../style/logoCo.css';
+import  '../../style/footer.css';
 import validateInput from '../../shared/validations/login';
 //import { connect  } from 'react-redux';
 import Request from 'superagent' ;
 import {store} from '../../store';
 import {push} from 'react-router-redux';
 
-
+import Img from 'react-image';
 
 class Login extends Component   {
 	constructor(props) {
 		super(props);
-		
+
 		this._changeUsername = this._changeUsername.bind(this);
 
 		this._changePassword = this._changePassword.bind(this);
@@ -27,19 +30,19 @@ class Login extends Component   {
 		this.props.onSubmit(this.props.formState);
 
 	}
-	
+
 	_changeUsername (event){
 		this._emitChange({...this.props.formState , username: event.target.value});
 	}
-	
+
 	_changePassword (event){
 		this._emitChange({...this.props.formState , password: event.target.value});
 	}
-	
+
 	_emitChange (newFormState){
 		this.props.changeForm(newFormState);
 	}
-	
+
 	_forgotten(e) {
 		store.dispatch(push('/Reset'));
 	}
@@ -47,6 +50,14 @@ class Login extends Component   {
 	render() {
 
 		return (
+				
+				
+				<div className="logoCompany">
+				<div id ="logoCo"> 
+				<Img src={logoCo} alt="logoCompany" width="300" height="200" />
+				</div> 
+				
+				
 
 				<div className="modal fade" id="login-modal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" >
 				<div className="modal-dialog">
@@ -64,6 +75,54 @@ class Login extends Component   {
 				</div>
 				</div>
 				</div> 
+				
+				<footer className="footer-distributed">
+
+				<div className="footer-left">
+
+					<div>
+						<i className="fa fa-map-marker"></i>
+						<p><span>4645 Boul Bourque </span> Sherbrooke, QC J1N 2G6</p>
+					</div>
+					
+					
+				</div>
+
+				<div className="footer-center">
+
+					
+
+					<div>
+						<i className="fa fa-phone"></i>
+						<p>+1 (819) 823-1443</p> 
+					</div>
+
+					
+
+				</div>
+
+				<div className="footer-right">
+
+					<div>
+						<i className="fa fa-envelope"></i>
+						<p><a href="mailto:crm.udes@gmail.com">crm.udes@gmail.com</a></p>
+					</div>
+
+					<div className="footer-icons">
+
+					
+						<a href="https://ca.linkedin.com/in/alainproulx"><i className="fa fa-linkedin"></i></a>
+						<a href="https://github.com/azizou69004/CRM-UDES-2017"><i className="fa fa-github"></i></a>
+
+					</div>
+
+				</div>
+				<p className="footer-copyright">  © 2017 CRM Application UDES. All rights reserved </p>
+			</footer>
+				
+				
+				</div> 
+				
 		);
 	}
 }
