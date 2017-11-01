b4cdf92e48f4b3d8ac55fb09cc173793834403bb
Aziz ZOUAOUI <zoua1703@usherbrooke.ca>
2017-11-01 08:55:20 -0400

    Reset password, amorce backend (fichier oublié a mon dernier commit)

--- crmApp-backend/crmApp-back/routes/resetPassword.js
+++ crmApp-backend/crmApp-back/routes/resetPassword.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
 var app = express();
+var nodemailer = require('nodemailer');
+var db = require('../models');
+var crypto = require('crypto');
+var {hashSync , genSaltSync} = require ('bcryptjs');
+var db = require('../models');
+var CryptoJS = require("crypto-js");
+var jwt = require('jsonwebtoken');
+var bcrypt = require ('bcryptjs');
+var security = require ('../security/security');
+
+/* GET Email reset logic && send email. */
 
 
-router.post('/resetPassword', function(req, res) {
+
+router.post('/ResetPassword', function(req, res) {
+
+	var newPasswordReceived = req.body.newPassword;
+	var confirmPasswordReceived = req.body.confirmPassword;
+
+	var decryptedPass=  CryptoJS.AES.decrypt(newPasswordReceived, 'secretKey24680');
+	var decryptedConf=  CryptoJS.AES.decrypt(confirmPasswordReceived, 'secretKey24680');
+
+
+	var mdpText = decryptedPass.toString(CryptoJS.enc.Utf8);
+	var mdpTextConf = decryptedConf.toString(CryptoJS.enc.Utf8);
+
+if(mdpText === mdpTextConf && mdpText.length === mdpTextConf.length ){
+	
+	console.log("mdpText  " ,mdpText) 
+
+	console.log("mdpTextConf  " ,mdpTextConf)
+	
+}
+else {
+	console.log("ERREUUUUUUR")
+
+//	gestion derreurs
+}
 
 });
 
