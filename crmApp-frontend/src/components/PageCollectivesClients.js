2d6bf4eb21739b911ae462412c9ae2178d2ab3e5
Aziz ZOUAOUI <zoua1703@usherbrooke.ca>
2017-11-01 23:02:08 -0400

    Assurances Collectives Redux Amorce Backend Ajout d'un state dossiersState pour la recuperation de listes de client depuis le back end

--- crmApp-frontend/src/components/PageCollectivesClients.js
+++ crmApp-frontend/src/components/PageCollectivesClients.js
@@ -2,21 +2,54 @@ import React, { Component } from 'react';
 import HistoriqueContainer from "../containers/HistoriqueContainer";
 import RechercheComponent from "./RechercheComponent";
 import {ButtonsComponent} from "./ButtonsComponent";
+import PageCollectives from '../components/PageCollectives';
+import { connect  } from 'react-redux';
+import {searchRequestColl, changeFormColl, getRequestClientColl} from '../actions/crmRechercheCollective';
+
+
 
 class PageCollectivesClients extends Component {
 	constructor(props) {
 		super(props);
 	}
     render() {
+		let { formState, changeForm } = this.props.crmRechercheCollective;
+
         return(
                 <div>
                     <h1>Assurances collectives</h1>
                     <h2>Clients</h2>
-                    <HistoriqueContainer page="PageCollectivesClients" history={this.props.history}/>
-                    <RechercheComponent/>
+                    <HistoriqueContainer page="PageCollectivesClients" history={this.props.history} />
+                    <RechercheComponent 
+                    onSubmit = {this.props.searchRequestColl}
+                	formState = {formState}
+                    changeFormColl = {this.props.changeFormColl}   />
                     <ButtonsComponent page="PageCollectivesClients" />
-                </div>
+                    	
+                    	</div>
         );
     }
 }
-export default (PageCollectivesClients);
+
+function mapStateToProps (state) {
+	
+	return{
+		crmRechercheCollective: state.crmRechercheCollective
+	}
+}
+
+//fonctions
+const  mapDispatchToProps = (dispatch) => {
+	
+	return{
+		searchRequestColl: (formData) => {
+			dispatch(searchRequestColl(formData))
+		},
+		changeFormColl : (newFormState) => {
+			dispatch(changeFormColl(newFormState))
+		}		
+	}
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps) (PageCollectivesClients);
--- crmApp-frontend/src/components/RechercheComponent.js
+++ crmApp-frontend/src/components/RechercheComponent.js
@@ -5,7 +5,7 @@ import {store} from '../store';
 import {push} from 'react-router-redux';
 //import state  from '../reducer/crmRechercheCollective';
 import { connect  } from 'react-redux';
-import {searchRequest, changeForm} from '../actions/crmRechercheCollective'
+import {searchRequestColl, changeFormColl, getRequestClientColl} from '../actions/crmRechercheCollective'
 
 class RechercheComponent extends Component {
 	
@@ -43,7 +43,7 @@ class RechercheComponent extends Component {
 		this._emitChange({...this.props.formState , numeroPolice: event.target.value});
 	}
 	_emitChange (newFormState){
-		this.props.changeForm(newFormState);
+		this.props.changeFormColl(newFormState);
 	}
 	
 //	_forgotten(e) {
@@ -51,15 +51,14 @@ class RechercheComponent extends Component {
 //	}
 	
     render() {
-		let { formState, changeForm } = this.props.crmRechercheCollective;
 
         return(
             <form action="" id="recherche" style={{display: 'flex', justifyContent: 'flex-start', textAlign: 'left'}}>
-                <input type="text" placeholder="Nom entreprise" onChange={this._changeNomEntreprise} value={formState.nomEntreprise} />
-                <input type="text" placeholder="Nom employé" onChange={this._changeNomEmploye}  value={formState.nomEmploye} />
-                <input type="text" placeholder="N° police"onChange={this._changeNumeroPolice} value={formState.numeroPolice}/>
-                <input type="text" placeholder="Mois renouvellement" value={formState.numeroPolice}/>
-                <input type="text" placeholder="Assureur" onChange={this._changeNomAssureur}  value={formState.nomAssureur}/>
+                <input type="text" placeholder="Nom entreprise" onChange={this._changeNomEntreprise} value={this.props.formState.nomEntreprise} />
+                <input type="text" placeholder="Nom employé" onChange={this._changeNomEmploye}  value={this.props.formState.nomEmploye} />
+                <input type="text" placeholder="N° police"onChange={this._changeNumeroPolice} value={this.props.formState.numeroPolice}/>
+                <input type="text" placeholder="Mois renouvellement" value={this.props.formState.numeroPolice}/>
+                <input type="text" placeholder="Assureur" onChange={this._changeNomAssureur}  value={this.props.formState.nomAssureur}/>
                 <select>
                     <option value="actif">Actif</option>
                     <option value="annulé">Annulé</option>
@@ -74,25 +73,4 @@ class RechercheComponent extends Component {
     }
 }
 
-function mapStateToProps (state) {
-	
-	return{
-		crmRechercheCollective: state.crmRechercheCollective
-	}
-}
-
-//fonctions
-const  mapDispatchToProps = (dispatch) => {
-	
-	return{
-		searchRequest: (formData) => {
-			dispatch(searchRequest(formData))
-		},
-		changeForm : (newFormState) => {
-			dispatch(changeForm(newFormState))
-		}		
-	}
-}
-
-
-export default connect(mapStateToProps, mapDispatchToProps) (RechercheComponent);
+export default (RechercheComponent   )
