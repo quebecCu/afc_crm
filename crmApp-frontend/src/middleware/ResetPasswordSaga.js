8fc5b6971f173790d1a9270047da68eb5b903603
roymath510 <roymath510@gmail.com>
2017-11-10 17:35:06 -0500

    Reset password avec le lien/token
    Add the base of the processing for the ResetPassword
    Put up stub to test the workflow
    Need to replace with true code

--- crmApp-frontend/src/middleware/ResetPasswordSaga.js
+++ crmApp-frontend/src/middleware/ResetPasswordSaga.js
@@ -1,54 +1,47 @@
-
 /** Imports **/
-//importer le salt pour le username et password
-//import genSalt from '../salt';
 import axios from 'axios';
 import CryptoJS from 'crypto-js';
 import {push} from 'react-router-redux';
 
-import {fork, put, take} from 'redux-saga/effects';
-import {RESET_PASSWORD_REQUEST, SENDING_PASSWORD_REQUEST} from '../actions/crmResetPassword';
-
 import 'react-s-alert/dist/s-alert-default.css';
+
+import {fork, put, take} from 'redux-saga/effects';
+import {RESET_PASSWORD_REQUEST, sendingRequest} from '../actions/crmResetPassword';
 import {store} from '../store';
 
 export function* resetPasswordFlow() {
+
 	while (true) {
 
 		let reset = yield take(RESET_PASSWORD_REQUEST);
 		let {newPassword, confirmPassword} = reset.data;
-		let token = reset.token;
+		let resetToken = reset.token;
 
-		yield put({type: SENDING_PASSWORD_REQUEST, sending: true});
+		yield put(sendingRequest(true));
 
 		let encryptedNewPassword = CryptoJS.AES.encrypt(newPassword, "secretKey24680").toString();
 		let encryptedConfirmPassword = CryptoJS.AES.encrypt(confirmPassword, "secretKey24680").toString();
 
-		// TODO: Add a .ENV file
+		// TODO: Add a .ENV file?
 		let server = "http://localhost:3002/ResetPassword";
 		//changer la location de la variable server pour plus de securite
 
 		axios.post(server, {
 			confirmPassword: encryptedConfirmPassword,
 			newPassword: encryptedNewPassword,
-			token: token
-
-		})
-			.then(function (response) {
-
-				console.log(response.data.emailSent);
-
-				if (!!response.data && response.data.status === "success") {
-					alert("votre MP a été changé");
-					store.dispatch(push('/'));
-				}
-				else {
-					// rajouter une alert
-					alert("MP non changé il y a eu un pblm etc ");
-				}
+			resetToken: resetToken
+		}).then((response) => {
+			if (!!response.data && response.data.status === 'success') {
+				alert("Votre mot de passe a été changé");
+				store.dispatch(push('/'));
+			}
+			else {
+				// rajouter une alert
+				alert('Mot de passe non changé\n\n' + response.data.message);
+			}
 //			callbackResponse: {response.data.res}
-			})
-			.catch(function (error) {
+		})
+			.catch((error) => {
 				console.log(error);
 			});
 	}
