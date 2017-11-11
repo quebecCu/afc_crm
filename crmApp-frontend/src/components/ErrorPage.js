487504255d00a16bc663f7f80f9e67187bfa11f1
Aziz ZOUAOUI <zoua1703@usherbrooke.ca>
2017-11-10 23:41:18 -0500

    ERROR 404 CSS

--- crmApp-frontend/src/components/ErrorPage.js
+++ crmApp-frontend/src/components/ErrorPage.js
@@ -10,23 +10,24 @@ class ErrorPage extends Component   {
 	constructor(props) {
 		super(props);
 
- 
+
 	}
-	 
+
 	render() {
 
 		return (
-
-				<div className="wrap">
+				<div id="404">
+				<div className="wrap" >
 				<div className="content">
-					<div className="logo">
-						<h1><a href="#"><img src={logo}/></a></h1>
-						<span><img src={signalImage}/>Oops! The Page you requested was not found!</span>
-					</div>
-					<h2>Cliquez <span><u> ici </u></span> pour retourner a la page principale</h2>
+				<div className="logo">
+				<h1><a href="#"><img src={logo}/></a></h1>
+				<span><img src={signalImage}/>Oops! la page demandée est en construction</span>
 				</div>
-			</div>
-				
+				<h2><u> RETOUR </u></h2>
+				</div>
+				</div>
+				</div>
+
 		);
 	}
 }
