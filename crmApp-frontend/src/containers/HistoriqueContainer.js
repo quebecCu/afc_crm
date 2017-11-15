61eef23955c9daf0ab247a746307b66ee53e6f32
Aziz ZOUAOUI <zoua1703@usherbrooke.ca>
2017-11-14 23:41:36 -0500

    [Sorting list client]    [filter default] Click sur Npm entreprise etc ... engendre le filtre & sorting

--- crmApp-frontend/src/containers/HistoriqueContainer.js
+++ crmApp-frontend/src/containers/HistoriqueContainer.js
@@ -9,9 +9,72 @@ class HistoriqueContainer extends Component {
         super(props);
         this.state = { historique: [{date: "20/09/2017", nom: "Kim", type: "Individuelle"},{date: "18/09/2017", nom: "Co", type: "Collective"}],
             };
+        this.handleClick0 = this.handleClick0.bind(this);
+        this.handleClick1 = this.handleClick1.bind(this);
+        this.handleClick2 = this.handleClick2.bind(this);
+        this.handleClick3 = this.handleClick3.bind(this);
+        this.handleClick4 = this.handleClick4.bind(this);
+        this.handleClick5 = this.handleClick5.bind(this);
+        this.handleClick6 = this.handleClick6.bind(this);
         }
-
-
+    
+    handleClick0() {
+    	this.handleClick(0);
+    }
+    handleClick1() {
+    	this.handleClick(1);
+    }
+    handleClick2() {
+    	this.handleClick(2);
+    }
+    handleClick3() {
+    	this.handleClick(3);
+    }
+    handleClick4() {
+    	this.handleClick(4);
+    }
+    handleClick5() {
+    	this.handleClick(5);
+    }
+    handleClick6() {
+    	this.handleClick(6);
+    }
+    handleClick(n) {
+    	  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    	  table = document.getElementById("PageCollectivesClientsTable");
+    	  switching = true;
+    	  dir = "asc"; 
+    	  while (switching) {
+    	    switching = false;
+    	    rows = table.getElementsByTagName("TR");
+    	    for (i = 1; i < (rows.length - 1); i++) {
+    	      shouldSwitch = false;
+    	      x = rows[i].getElementsByTagName("TD")[n];
+    	      y = rows[i + 1].getElementsByTagName("TD")[n];
+    	      if (dir == "asc") {
+    	        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+    	          shouldSwitch= true;
+    	          break;
+    	        }
+    	      } else if (dir == "desc") {
+    	        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+    	          shouldSwitch= true;
+    	          break;
+    	        }
+    	      }
+    	    }
+    	    if (shouldSwitch) {
+    	      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+    	      switching = true;
+    	      switchcount ++; 
+    	    } else {
+    	      if (switchcount == 0 && dir == "asc") {
+    	        dir = "desc";
+    	        switching = true;
+    	      }
+    	    }
+    	  }
+    	}
     render() {
         switch (this.props.page) {
             case 'PageAccueil':
@@ -34,13 +97,13 @@ class HistoriqueContainer extends Component {
                     <table id="PageCollectivesClientsTable" className="table">
                         <thead>
                         <tr>
-                            <th>Nom de l'entreprise</th>
-                            <th>Nom employé</th>
-                            <th>N° de police</th>
-                            <th>Mois de renouvellement</th>
-                            <th>Assureur</th>
-                            <th>Statut</th>
-                            <th>Prospect</th>
+                            <th onClick={this.handleClick0}>Nom de l'entreprise</th>
+                            <th onClick={this.handleClick1}>Nom employé</th>
+                            <th onClick={this.handleClick2}>N° de police</th>
+                            <th onClick={this.handleClick3}>Mois de renouvellement</th>
+                            <th onClick={this.handleClick4}>Assureur</th>
+                            <th onClick={this.handleClick5}>Statut</th>
+                            <th onClick={this.handleClick6}>Prospect</th>
                         </tr>
                         </thead>
                         <DossiersComponent collective={this.props.dossiersState} history={this.props.history} handleClick={this.props.changeViewDashboard}/>
