3e7fcee920198d488cb66f20c74aadfce60b64f7
Aziz ZOUAOUI <zoua1703@usherbrooke.ca>
2017-11-18 21:20:33 -0500

    [gestion user]

--- crmApp-frontend/src/containers/CreateUser.js
+++ crmApp-frontend/src/containers/CreateUser.js
@@ -2,23 +2,86 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {FormCreateUser} from '../components/FormCreateUser.js';
 import '../style/CreationUser.css';
-import {changeFormCreateUser} from "../actions/crmCreateUser";
 
-class createUser extends React.Component{
+import {changeFormCreateUser, submitUser, getRoles, getOperations, getDefaultPerms, updateUserPerms, updateUser} from "../actions/crmCreateUser";
+import CreateUserPermissions from './CreateUserPermissions';
+
+
+class CreateUser extends React.Component{
 
     constructor(props){
         super(props);
+        this.props.getOperations();
+        this.props.getDefaultPerms();
+        this.props.getRoles();
+        this.handleClick = this.handleClick.bind(this);
+		if(this.props.view === "UpdateUser"){
+			let newRole = JSON.parse(JSON.stringify(this.props.user.role));
+			let newName = JSON.parse(JSON.stringify(this.props.user.lastname));
+			let newLastName = JSON.parse(JSON.stringify(this.props.user.name));
+			let newLogin = JSON.parse(JSON.stringify(this.props.user.login));
+			let newMail = JSON.parse(JSON.stringify(this.props.user.mail));
+			let newUserPerms = JSON.parse(JSON.stringify(this.props.user.userPerms));
+			let userID=JSON.parse(JSON.stringify(this.props.user.id));
+			this.props.changeForm({
+				id:userID,
+				role: newRole,
+				nom: newName,
+				prenom:newLastName,
+				login: newLogin,
+				mail: newMail,
+				userPerms: newUserPerms,
+				roles:[],
+				operations:[],
+				mdpProv:''
+			});
+		}
+		else{
+			this.props.changeForm({...this.props.formState,
+				role: '',
+				nom: '',
+				prenom:'',
+				login: '',
+				mail: '',
+				mdpProv: '',
+				userPerms: [],
+				roles:[],
+				operations:[],
+			});
+		}
 
     }
 
 
 
-    render(){
-        let {formState} = this.props.crmCreateUser;
-        return <div><h1>Créer utilisateur</h1>
-            <FormCreateUser formState={formState}
-                             changeForm={this.props.changeForm}/>
+	handleClick(event){
+		let {formState} = this.props.crmCreateUser;
+		if(this.props.view === "CreateUser"){
+			console.log('formstate'+formState);
+			this.props.submitUser(formState);
+		}
+		else{//Cas où on est en modification de l'utilisateur
+    		this.props.updateUser(formState);
+		}
+		this.props.changeView("");
 
+
+	}
+
+    render() {
+        let {formState} = this.props.crmCreateUser;
+        return <div><div className="page-header" style={{paddingBottom:20}}><h1>{this.props.title}</h1></div>
+            <FormCreateUser formState={formState}
+							view={this.props.view}
+							user={this.props.user}
+                            changeForm={this.props.changeForm}
+                            submitUser={this.props.submitUser}/>
+            <CreateUserPermissions formState={formState}
+								   view={this.props.view}
+								   user={this.props.user}
+                                   changeForm={this.props.changeForm}
+								   updateUserPerms={this.props.updateUserPerms}/>
+			<button onClick={this.handleClick}>{this.props.button}</button>
         </div>;
     }
 }
@@ -36,8 +99,26 @@ const  mapDispatchToProps = (dispatch) => {
 
         changeForm : (newFormState) => {
             dispatch(changeFormCreateUser(newFormState))
-        }
+        },
+        submitUser : (formData) => {
+            dispatch(submitUser(formData))
+        },
+        getOperations: () =>{
+            dispatch(getOperations())
+        },
+        getDefaultPerms: () => {
+            dispatch(getDefaultPerms())
+        },
+        updateUserPerms: (position, newValue) =>{
+            dispatch(updateUserPerms(position, newValue))
+        },
+        getRoles : () => {
+            dispatch(getRoles())
+        },
+		updateUser: (updatedUser) =>{
+			dispatch(updateUser(updatedUser));
+		}
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps) (createUser);
+export default connect(mapStateToProps, mapDispatchToProps) (CreateUser);
--- crmApp-frontend/src/containers/GestionUser.js
+++ crmApp-frontend/src/containers/GestionUser.js
@@ -1,65 +1,81 @@
 import React from 'react';
 import '../style/PageAccueil.css';
-import {ListUsers} from '../components/ListUsers.js';
-import {store} from '../store';
-import {push} from 'react-router-redux';
+import {connect} from "react-redux";
+import ListUsers from '../components/ListUsers.js';
+import CreateUser from './CreateUser';
+import DisplayUser from './DisplayUser';
+import {changeViewUserManagement, getListUser, requestUserById, deleteUser} from "../actions/crmUserManagement";
+import {getOperations} from "../actions/crmCreateUser";
 
-export class gestionUser extends React.Component{
+class GestionUser extends React.Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            users:[
-                {
-                    id: 0,
-                    login: "AProulx",
-                    nom:"Proulx",
-                    prenom:"Alain",
-                    role:"admin",
-                    droits:["lecture","ecriture"]
-                },
-                {
-                    id: 1,
-                    login: "MChantal",
-                    nom:"Mercier",
-                    prenom:"Chantal",
-                    role:"stagiaire",
-                    droits:["lecture"]
-                },
-                {
-                    id: 2,
-                    login: "PVlad",
-                    nom:"Poutine",
-                    prenom:"Vladimir",
-                    role:"admin",
-                    droits:["lecture","ecriture"]
-                }
+	constructor(props) {
+		super(props);
+		this.props.getListUser();
+	}
+	
 
-            ]
+	render() {
+		let {view, formState} = this.props.crmUserManagement;
+		return <div id="UserManagement" >
+			<div className="view text-center" style={{ overflow: 'auto'}}>
+				{
+					view === "" && <ListUsers formState={formState} handleClick={this.props.changeViewUserManagement}
+											  displayUser={this.props.requestUserById}/>
+				}
+				{
+					view === "CreateUser" && <CreateUser changeView={this.props.changeViewUserManagement}
+														view={view}
+														  title="Création d'un utilisateur"
+														button="Créer l'utilisateur"/>
+				}
+				{
+					view === "DisplayUser"  && <DisplayUser formState={formState} user={formState.userToDisplay}
+															changeView={this.props.changeViewUserManagement}
+															getOperations={this.props.getOperations}
+															deleteUser={this.props.deleteUser}/>
+				}
+				{
+					view === "UpdateUser" && <CreateUser changeView={this.props.changeViewUserManagement}
+														 updateUser={this.props.updateUser}
+														 user={formState.userToDisplay}
+														 formState={formState}
+														 view={view}
+															title="Modification d'un utilisateur"
+															button="Modifier l'utilisateur"/>
+				}
+			</div>
+		</div>
+	}
+}
 
-        }
+function mapStateToProps (state) {
 
-    }
-
-
-    render(){
-        return <div><table className="user">
-            <thead>
-                <tr>
-                    <th>Username</th>
-                    <th>Nom prénom</th>
-                    <th>Rôle</th>
-                </tr>
-            </thead>
-            <ListUsers users={this.state.users}/>
-
-        </table>
-
-
-
-        <button onClick={(e) => store.dispatch(push("/PageAccueil/admin/createUser"))}>Créer utilisateur</button>
-
-        </div>;
-
+    return{
+        crmUserManagement: state.crmUserManagement
     }
 }
+
+//fonctions
+const  mapDispatchToProps = (dispatch) => {
+
+    return{
+        changeViewUserManagement: (newView) => {
+            dispatch(changeViewUserManagement(newView));
+        },
+		getListUser: () => {
+        	dispatch(getListUser());
+		},
+		requestUserById: (id) => {
+        	dispatch(requestUserById(id));
+		},
+		getOperations: () => {
+        	dispatch(getOperations());
+		},
+		deleteUser: (id) =>{
+        	dispatch(deleteUser(id));
+		}
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps) (GestionUser);
