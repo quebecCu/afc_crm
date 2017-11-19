e0beef88917892e689f7d911d0bd357e2c2dc71d
roymath510 <roymath510@gmail.com>
2017-11-18 19:09:51 -0500

    [RESET Password]
    Fix the tokenValidation
    Small code cleanup

--- crmApp-backend/crmApp-back/routes/resetPassword.js
+++ crmApp-backend/crmApp-back/routes/resetPassword.js
@@ -1,103 +1,116 @@
 'use strict';
 
 const CryptoJS = require("crypto-js");
+const {hashSync, genSaltSync} = require('bcryptjs');
 const express = require('express');
 const router = express.Router();
-var db = require('../models');
-var squelb = require('squel');
-var squel = squelb.useFlavour('postgres');
-var {hashSync , genSaltSync} = require ('bcryptjs');
-
+const db = require('../models');
+const squelb = require('squel');
+const squel = squelb.useFlavour('postgres');
 
 /**
  * Route serving password reset
  * @method POST
  * @URL /ResetPassword
- * @DataParams { newPassword: [string], confirmPassword: [string], token: [string] }
+ * @DataParamsInReqBody { newPassword: [string], confirmPassword: [string], token: [string] }
  * @SuccessResponse { status: 'success', message: [string]}
  * @ErrorResponse { status: 'fail', message: [string] }
  * **/
 router.post('/ResetPassword', async function (req, res) {
 
-	const newPwd = req.body.newPassword;
-	const newPwdConf = req.body.confirmPassword;
-	const resetToken = req.body.resetToken;
-
 	// Check all the params are NOT undefined
-	if (!(!!newPwd && !!newPwdConf && !!resetToken)) {
+	if (isValuesUndefined(req)) {
 		sendResponse("DEBUG|resetPassword|atLeastOneParamIsUndefined", res, "fail");
 		return;
 	}
+
+	const resetToken = req.body.resetToken;
+
 	// Check if the token is NOT valid
-	if (!(await !isTokenValid(req.body.resetToken))) {
+	if (await isTokenInvalid(resetToken)) {
 		sendResponse("DEBUG|resetPassword|theTokenIsNotValid", res, "fail");
 		return;
 	}
 
 	// Decrypt the password
-	const decryptedPass = CryptoJS.AES.decrypt(newPwd, 'secretKey24680');
-	const decryptedConf = CryptoJS.AES.decrypt(newPwdConf, 'secretKey24680');
+	const decryptedPass = CryptoJS.AES.decrypt(req.body.newPassword, 'secretKey24680');
+	const decryptedConf = CryptoJS.AES.decrypt(req.body.confirmPassword, 'secretKey24680');
+
 	// Make the password into a encoded uft8 string
 	const pwdText = decryptedPass.toString(CryptoJS.enc.Utf8);
 	const pwdTextConf = decryptedConf.toString(CryptoJS.enc.Utf8);
-	
-	let salt = genSaltSync (10);
-	let hashedPassword = hashSync(pwdText, salt);
+
 	// Check if the password match
 	if (pwdText !== pwdTextConf) {
 		sendResponse("DEBUG|resetPassword|passwordDoesn'tMatches", res, "fail");
 		return;
 	}
 
-	// The password match, update the pwd in the database
-	if (!(await !isTokenValid(req.body.resetToken))){
-		db.multi(squel.select()
-			.from('users."UTILISATEUR"')
-			.field("resetpasswordexpires")
-			.where("resetpasswordtoken = " + "'" + resetToken + "'")
-			.toString())
-			.spread(function (user) {
-				let _userTime = user[0].resetpasswordexpires
-				console.log("_checkChrono  " + _userTime)
-				let _currentTime = Date.now();
-				if( _currentTime < _userTime){
-					let _updateBD = db.one(squel.update()
-							.table('users."UTILISATEUR"')
-							.set("password", hashedPassword)
-							.where("resetpasswordtoken = " + "'" + resetToken + "'")
-							.set("resetpasswordtoken", null)
-							.set("resetpasswordexpires", null)
-							.returning('*')
-							.toString())
-				}
-			})
+	// Hash and salt the new password
+	let salt = genSaltSync(10);
+	let hashedPassword = hashSync(pwdText, salt);
 
-	sendResponse('DEBUG|resetPassword|passwordReset|STUB', res, 'success');
-	}
-	else {
-		sendResponse("DEBUG|resetPassword|theTokenIsNotValid", res, "fail");
-		return;
-	}
+	// Save the new password on the database
+	saveNewPassword(hashedPassword, resetToken);
+
+	// TODO: Should probably check if the db fail
+
+	// Success, the password as been updated
+	sendResponse('DEBUG|resetPassword|passwordSuccessfullyReset', res, 'success');
 });
 
 /**
- * Check if the token exist and is still valid
- * @param resetToken
+ * Check if the expected values are undefined
+ * @param req
  * @returns {boolean}
  */
-async function isTokenValid(resetToken) {
-	console.log("test");
+function isValuesUndefined (req){
+	return !(!!req.body.newPassword && !!req.body.confirmPassword && !!req.body.resetToken);
+}
+
+/**
+ * Check if the token is invalid
+ * @param resetToken
+ * @returns {Promise.<boolean>}
+ */
+async function isTokenInvalid (resetToken) {
+	return db.query(squel.select()
+		.from('users."UTILISATEUR"')
+		.field("resetpasswordexpires")
+		.where("resetpasswordtoken = " + "'" + resetToken + "'")
+		.toString())
+		.then((user) => {
+			// Check if a user as been found, a token exist and is not expired
+			return !(user.length > 0 && !!user[0].resetpasswordexpires && user[0].resetpasswordexpires > (new Date).getTime());
+		});
+}
+
+/**
+ * Save the new password on the Database
+ * @param hashedPassword
+ * @param resetToken
+ */
+function saveNewPassword(hashedPassword, resetToken) {
 	db.multi(squel.select()
-			.from('users."UTILISATEUR"')
-			.field("resetpasswordexpires")
-			.where("resetpasswordtoken = " + "'" + resetToken + "'")
-			.toString())
-			.spread(function (user) {
-				if(user.length > 0)
-					if(!!user[0].resetpasswordexpires)
-						return true;
-			
-			})}
+		.from('users."UTILISATEUR"')
+		.field("resetpasswordexpires")
+		.where("resetpasswordtoken = " + "'" + resetToken + "'")
+		.toString())
+		.spread((user) => {
+			const _userTime = user[0].resetpasswordexpires;
+			const _currentTime = Date.now();
+			if (_currentTime < _userTime) {
+				db.one(squel.update()
+					.table('users."UTILISATEUR"')
+					.set("password", hashedPassword)
+					.where("resetpasswordtoken = " + "'" + resetToken + "'")
+					.set("resetpasswordtoken", null)
+					.set("resetpasswordexpires", null)
+					.returning('*')
+					.toString())
+			}
+		});
+}
 
 /**
  * Send a response to the POST
@@ -105,7 +118,7 @@ async function isTokenValid(resetToken) {
  * @param response
  * @param status
  */
-function sendResponse(message, response, status) {
+function sendResponse (message, response, status) {
 	response.send({
 		status: status,
 		message: message
