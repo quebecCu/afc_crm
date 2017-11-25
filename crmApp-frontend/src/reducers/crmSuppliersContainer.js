37cb19f62b28dfcffdc3a1548b7a8511e78cd19c
Aziz ZOUAOUI <zoua1703@usherbrooke.ca>
2017-11-25 10:26:34 -0500

    [bug fixGestion fournisseurs ajout Redux FRONT ]

--- crmApp-frontend/src/reducers/crmSuppliersContainer.js
+++ crmApp-frontend/src/reducers/crmSuppliersContainer.js
@@ -1,10 +1,22 @@
-let initialState = {};
+import {
+	CHANGE_VIEW_SUPP
+}  from '../actions/crmSuppliersContainer';
 
-export default function reducer(state = initialState, action) {
 
-	switch (action.type) {
-		default:
-			return state
+let initialState = {
+    view: '',
+    errors: '',
+};
 
-	}
+export default function reducer (state = initialState, action ){
+
+    switch (action.type){
+
+        case CHANGE_VIEW_SUPP:
+            return {...state ,view: action.newView , error:''};
+        default:
+            return state
+
+    }
+
 }
